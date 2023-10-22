28af1fe751af7a9ae58d144938a3a8342d64a0ad
jsilva92 <jeniffersilva92@hotmail.com>
2023-10-21 23:05:38 -0300

    criação de conteúdo

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+//As informações fixadas aqui devem vir de um banco de dados
+
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Título da Primeira Notícia (Content)",
+        "description": "descricao da primeira noticia: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "photoCover": "https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder-300x200.png"
+    },
+    {
+        "id": "2",
+        "title": "Título da Segunda Notícia (Content)",
+        "description": "descricao da segunda noticia: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "photoCover": "https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder-300x200.png"
+    }
+]
